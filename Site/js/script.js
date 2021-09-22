f8bf7bee39dbf9ae8af284c124ef5ecd7d0210a3
Alice Marques <alice_marquesa@hotmail.com>
2021-09-22 15:20:02 -0300

    pop-up funcionando com javascript

--- Site/js/script.js
+++ Site/js/script.js
@@ -1,15 +1,77 @@
-const exitMenu = document.getElementById('sair');
-const abrirMenu = document.getElementById('menu');
-const menu = document.getElementById('menuMobile');
+let exitMenu;
+let abrirMenu;
+let menu;
+let btnLogin;
+let btnCadastro;
+let wrapper;
+let loginPage;
+let cadastroPage;
+let exitPopup;
 
-abrirMenu.addEventListener('click', ()=> {
-if (menu.style.display === "block") {
+
+window.addEventListener('load', () => {
+
+  exitMenu = document.getElementById('sair');
+  abrirMenu = document.getElementById('menu');
+  menu = document.getElementById('menuMobile');
+
+  abrirMenu.addEventListener('click', () => {
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  })
+
+  exitMenu.addEventListener('click', () => {
     menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
+  })
+
+  // pop-up stuff
+
+  btnLogin = document.querySelector("#login");
+  btnCadastro = document.querySelector("#cadastro");
+
+  wrapper = document.querySelector(".pop-up-wrapper");
+  loginPage = document.querySelector(".pop-up.login");
+  cadastroPage = document.querySelector(".pop-up.register");
+  exitPopup = document.querySelectorAll(".pop-up img:first-of-type")
+
+  btnLogin.onclick = (event) => {
+    loginToggle()
   }
+
+  btnCadastro.onclick = (event) => {
+    cadastroToggle()
+  }
+
+  wrapper.onclick = (event) => {
+    if (event.target == wrapper) {
+      closePopup()
+    } 
+    else
+    exitPopup.forEach(exit => {
+      if (event.target == exit) {
+        event.preventDefault();
+        closePopup();
+      }
+    });
+  }
+
 })
 
-exitMenu.addEventListener('click', ()=> {
-    menu.style.display = "none";
-})
+function loginToggle() {
+  wrapper.classList.toggle("active");
+  loginPage.classList.toggle("active");
+}
+
+function cadastroToggle() {
+  wrapper.classList.toggle("active");
+  cadastroPage.classList.toggle("active");
+}
+
+function closePopup() {
+  wrapper.classList.remove("active");
+  loginPage.classList.remove("active");
+  cadastroPage.classList.remove("active");
+}
