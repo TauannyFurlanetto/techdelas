8b4527a2baebfa1943a3f0066b13f2862d5c0a40
Vanessa-Ro <vanessarodriguescardoso8@gmail.com>
2021-10-01 15:05:27 -0300

    Alterações barra de pesquisa

--- Site/js/pesquisa.js
+++ Site/js/pesquisa.js
@@ -6,16 +6,18 @@ function pesquisar(){
     menu = document.querySelector('article.janela');
     menuItens = menu.querySelectorAll('h3');
     menuItens2 = menu.querySelectorAll('a');
+    vejaMais = document.getElementById('veja-mais');
 
     for(let i=0; i<menuItens.length; i++){
         let links = menuItens[i];
             
-
         if(links.innerHTML.toUpperCase().includes(filtro)){
             menuItens2[i].style.display='block';
-            console.log(menuItens2[i])
+            vejaMais.style.display='none';
+
         }else{
             menuItens2[i].style.display='none';
+            vejaMais.style.display='block';
         }
             
      }
