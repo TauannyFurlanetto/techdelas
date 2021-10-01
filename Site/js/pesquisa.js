0b4b7e18de17b3f25b779932152f2ce0fe301b86
Tauanny Furlanetto <tauannyvfs@gmail.com>
2021-10-01 18:09:24 -0300

    Categorizacao pesquisa

--- Site/js/pesquisa.js
+++ Site/js/pesquisa.js
@@ -24,3 +24,37 @@ function pesquisar(){
      }
 }
 
+let toogle = true;
+let arrCat = ["Tecnologia", "Empoderamento", "Ciencia", "Educacao", "Saude"];
+const catList = document.querySelectorAll(".categorias ul li");
+catList.forEach(ele=>{
+    ele.style.fontWeight = "500"
+})
+function selectCat(cat, txt){
+    cat = arrCat[cat];
+    if (txt.style.fontWeight == "500"){
+        txt.style.fontWeight = "600";
+    }else{
+        txt.style.fontWeight = "500"
+    }
+    let vejaMais = document.getElementById('veja-mais');
+    const posts = document.querySelectorAll(".Cat");
+        console.log("click");
+        if (toogle){
+            console.log("if" + toogle);
+            posts.forEach(element=>{
+                if(!element.className.includes(cat)){
+                    element.style.display = "none";
+                    toogle = false;
+                } 
+            })
+            vejaMais.style.display = "none";
+        }else{
+            console.log("else"+ toogle);
+            posts.forEach(element=>{
+                element.style.display = "flex";
+                toogle = true;
+            })
+            vejaMais.style.display = "block";
+        };   
+}
