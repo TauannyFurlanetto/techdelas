68503fc3414326210c00f494f28243783622b5ba
Tauanny Furlanetto <tauannyvfs@gmail.com>
2021-10-13 17:29:47 -0300

    Iniciando a página de podcasts

--- Site/src/index.js
+++ Site/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './Home';
-
+import Podcasts from './Podcasts';
+import Podcast from './components/Podcast.js';
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Podcasts />
   </React.StrictMode>,
   document.getElementById('root')
 );
