26fd094b4325b8b33b8cab18da098b55d64ce891
Tauanny Furlanetto <tauannyvfs@gmail.com>
2021-10-14 15:12:54 -0300

    Popup no header

--- Site/src/components/header.js
+++ Site/src/components/header.js
@@ -1,7 +1,10 @@
 import logoHeader from "../icones/logo_header.svg";
 import exitMobile from "../icones/exit.svg";
+import email from "../icones/email.svg"
+import password from "../icones/senha.svg";
 export default function Cabecalho () {
     return (
+        <>
         <header>
             <div class="container">
                 <section class="logo">
@@ -27,5 +30,92 @@ export default function Cabecalho () {
                 </section>
             </div>
         </header>
+        {/* <!-- POP-UP --> */}
+
+        <div className="pop-up-wrapper">
+            <div className="pop-up register">
+
+                <a href=""><img src={exitMobile} alt="" /></a>
+
+                <h2>Cadastre-se:</h2>
+
+                <form>
+                    <div className="pop-up-labels nome">
+                        <label for-id="reg-nome">Nome</label>
+                        <p></p>
+                    </div>
+                    <input type="nome" id="reg-nome" name="nome"/>
+
+                    <div className="pop-up-labels email">
+                        <label for-id="reg-email">Email</label>
+                        <p></p>
+                    </div>
+                    <div className="input">
+                        <img src={email} alt="" />
+                        <input type="email" id="reg-email" name="email"/>
+                    </div>
+
+                    <div className="pop-up-labels senha">
+                        <label for-id="reg-senha">Senha</label>
+                        <p></p>
+                    </div>
+                    <div className="input">
+                        <img src={password} alt="" />
+                        <input type="password" id="reg-senha" name="senha"/>
+                    </div>
+
+                    <div className="pop-up-labels senha-confirm">
+                        <label for-id="reg-senha-confirm">Digite novamente sua senha</label>
+                        <p></p>
+                    </div>
+                    <div className="input">
+                        <img src={password} alt="" />
+                        <input type="password" id="reg-senha-confirm" name="senha"/>
+                    </div>
+
+                    <div className="button">
+                        <button className="register">Cadastre-se</button>
+                    </div>
+                </form>
+
+            </div>
+
+            <div className="pop-up login">
+
+                <a href=""><img src={exitMobile} alt="" /></a>
+
+                <h2>Login:</h2>
+
+                <form>
+
+                    <div className="pop-up-labels email">
+                        <label for-id="login-email">Email</label>
+                        <p></p>
+                    </div>
+                    <div className="input">
+                        <img src={email} alt="" />
+                        <input type="email" id="login-email" name="email"/>
+                    </div>
+
+                    <div className="pop-up-labels senha">
+                        <label for-id="login-senha">Senha</label>
+                        <p></p>
+                    </div>
+                    <div className="input">
+                        <img src={password} alt="" />
+                        <input type="password" id="login-senha" name="senha"/>
+                    </div>
+
+                    <a className="esqueci" href="">Esqueci minha senha</a>
+
+                    <div className="button">
+                        <button className="login">Login</button>
+                    </div>
+                </form>
+
+            </div>
+
+        </div>
+        </>
     )
 }
