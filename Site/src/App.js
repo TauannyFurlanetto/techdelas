27ace0ae5a4abf90622d3be3e06c47c8ecd93748
Alice Marques <alice_marquesa@hotmail.com>
2021-10-15 15:34:25 -0300

    consertado o header dentro do browserrouter

--- Site/src/App.js
+++ Site/src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter} from 'react-router-dom'
 
 import Routes from './routes';
+import Cabecalho from './components/header';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Cabecalho />
         <Routes />
       </BrowserRouter>
     </div>
