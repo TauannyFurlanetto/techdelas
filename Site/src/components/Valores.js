18a08c0df6a893bc30cfa01c36c88a1dafd2daa1
Vanessa-Ro <vanessarodriguescardoso8@gmail.com>
2021-10-19 16:06:30 -0300

    Iniciando componente de Valores - Escreva

---
+++ Site/src/components/Valores.js
@@ -0,0 +1,13 @@
+function Valores (props) {
+    return (
+        <>
+        <li><img src={props.imagem} alt="ícone braço"/>{props.titulo}</li>
+        {/* <li><img src={iconeLampada} alt="ícone lâmpada"/>Inspirar</li>
+        <li><img src={iconeEscala} alt="Ícone escala gráfica"/>Crescer e evoluir juntas</li>
+        <li><img src={iconeMaoCoracao} alt="Ícone mão segurando coração"/>Incluir</li>
+        <li><img src={iconeCoracao} alt="Ícone de coração"/>Paixão por realizar</li> */}
+        </>
+    )
+}
+
+export default Valores
