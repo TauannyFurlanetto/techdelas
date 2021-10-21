643b9c88b00edc4c943140a86ba4b9d111688446
Tauanny Furlanetto <tauannyvfs@gmail.com>
2021-10-21 15:54:08 -0300

    Criando o post de engenharia quimica

---
+++ Site/src/posts/artigo-eng.js
@@ -0,0 +1,32 @@
+const post = {
+    titulo: "Mulheres revolucionárias na Engenharia Química",
+    classes: "Ciencia",
+    subtitulo: "Inspirações femininas na área, desafios e motivações",
+    img: "post2.jpg",
+    alt: "Imagem retirada de pexels",
+    url: "artigo-eng",
+    autor: "Maria Lima",
+    data: "10/07/2021",
+
+    content: [
+        {img: {
+            source: "img/artigo2.jpg",
+            alt: "Imagem retirada de pexels"
+        }},
+        {p: "Com o passar dos anos, as mulheres vem conquistando cada vez mais seu espaço no mercado de trabalho, tanto em pequenos cargos, como em cargos de liderança, abandonando o estilo “dona de casa” e embarcando para o ramo das finanças, da administração, da medicina e das engenharias."},
+        {p: "Esses dados servem para nos alertar a respeito da importância da discussão relacionada ao empoderamento da mulher na medicina. Vale mencionar que, na categoria “médicos”, há diferenças importantes relacionadas a gênero.A engenharia, por exemplo, vem passando por mudanças, uma delas é o aumento da participação feminina no setor. A cada ano, mais mulheres ingressam nos cursos de Engenharia."},
+        {p: "Cada vez mais mulheres escolhem a Engenharia como profissão e conquistam aos poucos seu espaço, trabalhando em parceria com os profissionais do sexo masculino."},
+        {p: "No entanto, essas conquistas ainda vêm acompanhadas de muitos desafios, como a condução com sucesso e responsabilidade de uma dupla jornada, já que muitas dessas engenheiras são esposas e mães, além de atuarem em cargos de liderança."},
+        {p: "Entretanto, essas responsabilidades e desafios não nos impedem de seguirmos adiante e darmos nosso melhor."},
+        {p: 'Baseando nisso tudo, selecionei 1 (uma) mulher que fez sucesso na  engenharia, para nos inspirar nos momentos difíceis e para nos mantermos sempre avançando.'},
+        {p: 'Edith Clarke inicialmente se graduou em matemática e astronomia (1908), onde lecionou matemática por 3 anos. Mas sua paixão pelas exatas fez com que em 1911, se matriculasse em engenharia mecânica na Universidade de Wisconsin em Nova York.'},
+        {p: 'Mais tarde, deixou Nova York para entrar no Instituto de Tecnologia de Massachusetts (MIT), onde obteve seu mestrado em engenharia elétrica em 1918, tornando-se a primeira mulher a ganhar um diploma nessa área no MIT.'},
+        {p: 'Após sua formação, Clarke trabalhou como engenheira da General Electric, onde desenvolveu uma “calculadora gráfica”. Este dispositivo foi usado para resolver problemas da linha de transmissão de energia elétrica.'},
+        {p: 'Após sua aposentadoria da General Electric em 1947, tornou-se professora de engenharia elétrica na Universidade do Texas, Austin. Ela se tornou a primeira mulher a ensinar no departamento de engenharia da Universidade do Texas.'},
+        {p: 'E em 1954, as realizações de Edith foram reconhecidas pela <em>“Society of Women Engineers Achievements Award”</em>, em funções das suas muitas contribuições originais para a teoria de estabilidade e análise de circuitos.'},
+        {p:'Mesmo diante de todas as adversidades, Edith Clarke nunca deixou de lutar por aquilo que amava. Pois afinal, em pleno século XXI, as mulheres ainda sofrem preconceito em sua área de formação, imaginem então em 1918, onde o ramo da engenharia era governado pelos homens.'},
+        {p:'No entanto, isso não fez Clarke desistir, muito pelo contrário, suas invenções fizeram a diferença no mundo da engenharia elétrica e para as mulheres e homens em todo o mundo.'},
+        {p:'Referências: 2 Engenheiros: 5 Mulheres que Revolucionaram a Engenharia.'}
+    ]
+}
+export default post
